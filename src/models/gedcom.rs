13cf3e264b588baae08dcdd5e6ffa81f56f5f36d
Donald Robertson <9719614+03k64@users.noreply.github.com>
2020-05-27 22:31:20 +0100

    Adding tests for change_node_to_date_time

--- src/models/gedcom.rs
+++ src/models/gedcom.rs
@@ -28,3 +28,281 @@ pub fn change_node_to_date_time(node: &GedcomTreeNode) -> Result<NaiveDateTime, 
 
     Ok(date_time)
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::models::gedcom::{
+        change_node_to_date_time, GedcomLine, GedcomLineTag, GedcomTreeNodeBuilder,
+    };
+    use chrono::{NaiveDate, NaiveDateTime, NaiveTime};
+
+    #[test]
+    fn test_change_node_to_date_time() {
+        let time = GedcomLine::builder()
+            .with_level(2)
+            .with_optional_line_value(Some(String::from("00:00:00")))
+            .with_tag(GedcomLineTag::Time)
+            .build()
+            .unwrap();
+        let time = GedcomTreeNodeBuilder::from(time).build().unwrap();
+
+        let date = GedcomLine::builder()
+            .with_level(1)
+            .with_optional_line_value(Some(String::from("1 Jan 1900")))
+            .with_tag(GedcomLineTag::Date)
+            .build()
+            .unwrap();
+        let date = GedcomTreeNodeBuilder::from(date)
+            .with_children(vec![time])
+            .build()
+            .unwrap();
+
+        let change = GedcomLine::builder()
+            .with_level(0)
+            .with_tag(GedcomLineTag::Change)
+            .build()
+            .unwrap();
+        let change = GedcomTreeNodeBuilder::from(change)
+            .with_children(vec![date])
+            .build()
+            .unwrap();
+
+        let actual = change_node_to_date_time(&change);
+        assert!(actual.is_ok());
+
+        let expected = NaiveDateTime::new(
+            NaiveDate::from_ymd(1900, 1, 1),
+            NaiveTime::from_hms(0, 0, 0),
+        );
+
+        let actual = actual.unwrap();
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn test_change_node_to_date_time_with_invalid_time_value() {
+        let time = GedcomLine::builder()
+            .with_level(2)
+            .with_optional_line_value(Some(String::from("INVALID_TIME")))
+            .with_tag(GedcomLineTag::Time)
+            .build()
+            .unwrap();
+        let time = GedcomTreeNodeBuilder::from(time).build().unwrap();
+
+        let date = GedcomLine::builder()
+            .with_level(1)
+            .with_optional_line_value(Some(String::from("1 Jan 1900")))
+            .with_tag(GedcomLineTag::Date)
+            .build()
+            .unwrap();
+        let date = GedcomTreeNodeBuilder::from(date)
+            .with_children(vec![time])
+            .build()
+            .unwrap();
+
+        let change = GedcomLine::builder()
+            .with_level(0)
+            .with_tag(GedcomLineTag::Change)
+            .build()
+            .unwrap();
+        let change = GedcomTreeNodeBuilder::from(change)
+            .with_children(vec![date])
+            .build()
+            .unwrap();
+
+        let actual = change_node_to_date_time(&change);
+        assert!(actual.is_err());
+
+        let expected = "Gedcom Change has invalid date_time";
+        let actual = actual.unwrap_err();
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn test_change_node_to_date_time_with_invalid_date_value() {
+        let time = GedcomLine::builder()
+            .with_level(2)
+            .with_optional_line_value(Some(String::from("00:00:00")))
+            .with_tag(GedcomLineTag::Time)
+            .build()
+            .unwrap();
+        let time = GedcomTreeNodeBuilder::from(time).build().unwrap();
+
+        let date = GedcomLine::builder()
+            .with_level(1)
+            .with_optional_line_value(Some(String::from("INVALID_DATE")))
+            .with_tag(GedcomLineTag::Date)
+            .build()
+            .unwrap();
+        let date = GedcomTreeNodeBuilder::from(date)
+            .with_children(vec![time])
+            .build()
+            .unwrap();
+
+        let change = GedcomLine::builder()
+            .with_level(0)
+            .with_tag(GedcomLineTag::Change)
+            .build()
+            .unwrap();
+        let change = GedcomTreeNodeBuilder::from(change)
+            .with_children(vec![date])
+            .build()
+            .unwrap();
+
+        let actual = change_node_to_date_time(&change);
+        assert!(actual.is_err());
+
+        let expected = "Gedcom Change has invalid date_time";
+        let actual = actual.unwrap_err();
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn test_change_node_to_date_time_with_invalid_date_time_value() {
+        let time = GedcomLine::builder()
+            .with_level(2)
+            .with_optional_line_value(Some(String::from("INVALID_DATE_TIME")))
+            .with_tag(GedcomLineTag::Time)
+            .build()
+            .unwrap();
+        let time = GedcomTreeNodeBuilder::from(time).build().unwrap();
+
+        let date = GedcomLine::builder()
+            .with_level(1)
+            .with_optional_line_value(Some(String::from("INVALID_DATE")))
+            .with_tag(GedcomLineTag::Date)
+            .build()
+            .unwrap();
+        let date = GedcomTreeNodeBuilder::from(date)
+            .with_children(vec![time])
+            .build()
+            .unwrap();
+
+        let change = GedcomLine::builder()
+            .with_level(0)
+            .with_tag(GedcomLineTag::Change)
+            .build()
+            .unwrap();
+        let change = GedcomTreeNodeBuilder::from(change)
+            .with_children(vec![date])
+            .build()
+            .unwrap();
+
+        let actual = change_node_to_date_time(&change);
+        assert!(actual.is_err());
+
+        let expected = "Gedcom Change has invalid date_time";
+        let actual = actual.unwrap_err();
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn test_change_node_to_date_time_with_no_time_value() {
+        let time = GedcomLine::builder()
+            .with_level(2)
+            .with_tag(GedcomLineTag::Time)
+            .build()
+            .unwrap();
+        let time = GedcomTreeNodeBuilder::from(time).build().unwrap();
+
+        let date = GedcomLine::builder()
+            .with_level(1)
+            .with_optional_line_value(Some(String::from("1 Jan 1900")))
+            .with_tag(GedcomLineTag::Date)
+            .build()
+            .unwrap();
+        let date = GedcomTreeNodeBuilder::from(date)
+            .with_children(vec![time])
+            .build()
+            .unwrap();
+
+        let change = GedcomLine::builder()
+            .with_level(0)
+            .with_tag(GedcomLineTag::Change)
+            .build()
+            .unwrap();
+        let change = GedcomTreeNodeBuilder::from(change)
+            .with_children(vec![date])
+            .build()
+            .unwrap();
+
+        let actual = change_node_to_date_time(&change);
+        assert!(actual.is_err());
+
+        let expected = "Gedcom Time has no value";
+        let actual = actual.unwrap_err();
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn test_change_node_to_date_time_with_no_time() {
+        let date = GedcomLine::builder()
+            .with_level(1)
+            .with_optional_line_value(Some(String::from("1 Jan 1900")))
+            .with_tag(GedcomLineTag::Date)
+            .build()
+            .unwrap();
+        let date = GedcomTreeNodeBuilder::from(date).build().unwrap();
+
+        let change = GedcomLine::builder()
+            .with_level(0)
+            .with_tag(GedcomLineTag::Change)
+            .build()
+            .unwrap();
+        let change = GedcomTreeNodeBuilder::from(change)
+            .with_children(vec![date])
+            .build()
+            .unwrap();
+
+        let actual = change_node_to_date_time(&change);
+        assert!(actual.is_err());
+
+        let expected = "Gedcom Date has no time";
+        let actual = actual.unwrap_err();
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn test_change_node_to_date_time_with_no_date_value() {
+        let date = GedcomLine::builder()
+            .with_level(1)
+            .with_tag(GedcomLineTag::Date)
+            .build()
+            .unwrap();
+        let date = GedcomTreeNodeBuilder::from(date).build().unwrap();
+
+        let change = GedcomLine::builder()
+            .with_level(0)
+            .with_tag(GedcomLineTag::Change)
+            .build()
+            .unwrap();
+        let change = GedcomTreeNodeBuilder::from(change)
+            .with_children(vec![date])
+            .build()
+            .unwrap();
+
+        let actual = change_node_to_date_time(&change);
+        assert!(actual.is_err());
+
+        let expected = "Gedcom Date has no value";
+        let actual = actual.unwrap_err();
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn test_change_node_to_date_time_with_no_date() {
+        let change = GedcomLine::builder()
+            .with_level(0)
+            .with_tag(GedcomLineTag::Change)
+            .build()
+            .unwrap();
+        let change = GedcomTreeNodeBuilder::from(change).build().unwrap();
+
+        let actual = change_node_to_date_time(&change);
+        assert!(actual.is_err());
+
+        let expected = "Gedcom Change has no date";
+        let actual = actual.unwrap_err();
+        assert_eq!(actual, expected);
+    }
+}
