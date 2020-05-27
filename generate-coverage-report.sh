2bd0668728d49f5a702252ed777f2655a05ae4c2
Donald Robertson <9719614+03k64@users.noreply.github.com>
2020-05-27 22:55:20 +0100

    Addition of script to generate a coverage report

---
+++ generate-coverage-report.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cargo clean
+
+export CARGO_INCREMENTAL=0
+export RUSTFLAGS="-Zprofile -Ccodegen-units=1 -Copt-level=0 -Clink-dead-code -Coverflow-checks=off -Zpanic_abort_tests -Cpanic=abort"
+export RUSTDOCFLAGS="-Cpanic=abort"
+
+cargo build
+cargo test
+grcov ./target/debug/ -s . -t html --llvm --branch --ignore-not-existing -o ./coverage/ --ignore src/main.rs
