b2e1e8ab62d96ac015ca5e168a758ab175cb3286
Donald Robertson <9719614+03k64@users.noreply.github.com>
2020-06-16 09:40:55 +0100

    Adding check for existing json files

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,8 @@ use std::{
     io::{Read, Write},
 };
 
+const JSON: &'static str = "json";
+
 fn main() -> Result<(), Box<dyn Error>> {
     let input_directory_arg = Arg::with_name("directory")
         .help("Specify a directory containing one or more GEDCOM files to convert")
@@ -31,16 +33,19 @@ fn main() -> Result<(), Box<dyn Error>> {
                 Err(_) => {}
                 Ok(entry) => {
                     let input_path = entry.path();
-                    let output_path = input_path.with_extension("json");
 
-                    let mut input = File::open(input_path)?;
-                    let mut gedcom = String::new();
-                    input.read_to_string(&mut gedcom)?;
+                    if input_path.extension().unwrap_or_default() != JSON {
+                        let output_path = input_path.with_extension("json");
 
-                    let json = gedcom_to_relation_json(gedcom.as_str())?;
+                        let mut input = File::open(input_path)?;
+                        let mut gedcom = String::new();
+                        input.read_to_string(&mut gedcom)?;
 
-                    let mut output = File::create(output_path)?;
-                    output.write_all(json.as_bytes())?;
+                        let json = gedcom_to_relation_json(gedcom.as_str())?;
+
+                        let mut output = File::create(output_path)?;
+                        output.write_all(json.as_bytes())?;
+                    }
                 }
             }
         }
